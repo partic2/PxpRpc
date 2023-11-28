86a645f6f3b4f516fa4ed4a728bd2e0db30b59ae
partic <1596067968@qq.com>
2023-11-28 21:45:11 +0800

    fix libuv backend read buffer

--- c/pxprpc_libuv/pxprpc_libuv.c
+++ c/pxprpc_libuv/pxprpc_libuv.c
@@ -33,7 +33,8 @@ static pxprpc_server_libuv pxprpc_new_libuvserver(uv_loop_t *loop,uv_stream_t *l
     self->acceptedConn=NULL;
     return self;
 }
-#define __sockconn_internalBuf_len 4096+8
+/* XXX: How large it should be?*/
+#define __sockconn_internalBuf_len 1024
 struct _pxprpc_libuv_sockconn{
     struct _pxprpc_libuv *serv;
     struct _pxprpc_libuv_sockconn *prev;
@@ -47,8 +48,8 @@ struct _pxprpc_libuv_sockconn{
     const char* writeError;
     uv_buf_t nextBuf;
     char internalBuf[__sockconn_internalBuf_len];
-    uint32_t bufStart;
-    uint32_t bufEnd;
+    uint16_t bufStart;
+    uint16_t bufEnd;
     int status;
 };
 
@@ -56,47 +57,26 @@ struct _pxprpc_libuv_sockconn{
 
 static void __sockMoveInternalBufToNextBuf(struct _pxprpc_libuv_sockconn *self){
     if(self->nextBuf.len>0){
-        if(self->bufEnd<self->bufStart){
-            int size=__sockconn_internalBuf_len-self->bufStart;
-            if(size>self->nextBuf.len){
-                size=self->nextBuf.len;
-            }
-            memcpy(self->nextBuf.base,self->internalBuf+self->bufStart,size);
-            self->nextBuf.len-=size;
-            self->nextBuf.base+=size;
-            self->bufStart+=size;
-            if(self->bufStart>=__sockconn_internalBuf_len){
-                self->bufStart-=__sockconn_internalBuf_len;
-            }
+        int size=self->bufEnd-self->bufStart;
+        if(size>self->nextBuf.len){
+            size=self->nextBuf.len;
         }
-    }
-    if(self->nextBuf.len>0){
-        if(self->bufStart<self->bufEnd){
-            int size=self->bufEnd-self->bufStart;
-            if(size>self->nextBuf.len){
-                size=self->nextBuf.len;
-            }
-            memcpy(self->nextBuf.base,self->internalBuf+self->bufStart,size);
-            self->nextBuf.len-=size;
-            self->nextBuf.base+=size;
-            self->bufStart+=size;
-        }
+        memmove(self->nextBuf.base,self->internalBuf+self->bufStart,size);
+        self->nextBuf.len-=size;
+        self->nextBuf.base+=size;
+        self->bufStart+=size;
     }
 }
 
 static void __sockUvAllocCb(uv_handle_t* handle,size_t suggested_size,uv_buf_t* buf){
     struct _pxprpc_libuv_sockconn *self=(struct _pxprpc_libuv_sockconn*)uv_handle_get_data(handle);
     if(self->nextBuf.len==0){
-        buf->base=self->internalBuf+self->bufEnd;
-        if(self->bufStart>self->bufEnd){
-            //remain 4 byte to avoid self->bufStart==self->bufEnd, which indicate buffer is empty.
-            buf->len=self->bufStart-self->bufEnd-4;
-        }else{
-            buf->len=__sockconn_internalBuf_len;
-            if(self->bufStart<4){
-                buf->len-=4-self->bufStart;
-            }
+        if(self->bufStart==self->bufEnd){
+            self->bufStart=0;
+            self->bufEnd=0;
         }
+        buf->base=self->internalBuf+self->bufEnd;
+        buf->len=__sockconn_internalBuf_len-self->bufEnd;
     }else{
         *buf=self->nextBuf;
     }
@@ -108,11 +88,8 @@ static void __sockUvReadCb(uv_stream_t* stream,ssize_t nread,const uv_buf_t* buf
             self->nextBuf.base+=nread;
             self->nextBuf.len-=nread;
         }else{
-            //read to internal buffer
+            /*read to internal buffer*/
             self->bufEnd+=nread;
-            if(self->bufEnd>=__sockconn_internalBuf_len){
-                self->bufEnd-=__sockconn_internalBuf_len;
-            }
             __sockMoveInternalBufToNextBuf(self);
         }
         if(self->nextBuf.len==0){
@@ -164,7 +141,7 @@ static void __sockAbsIo1Read(struct pxprpc_abstract_io *self1,uint32_t length,ui
     }
 }
 
-// consider cross thread request;
+/*XXX: consider cross thread request*/
 struct __sockUvWriteReq{
     uv_write_t uvReq;
     uv_buf_t buf;
@@ -266,7 +243,7 @@ static void __uvListenConnectionCb(uv_stream_t *server, int status){
 static void pxprpc_serve(pxprpc_server_libuv serv){
     struct _pxprpc_libuv *self=(struct _pxprpc_libuv *)serv;
     self->status|=status_RUNNING;
-    //XXX:Can we use user data of listener?
+    /*XXX:Can we use user data of listener?*/
     uv_handle_set_data((uv_handle_t *)self->listener,self);
     uv_listen(self->listener,8,__uvListenConnectionCb);
 }
