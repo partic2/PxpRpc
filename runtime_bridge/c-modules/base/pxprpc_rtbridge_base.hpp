997998bafc272206e867dd099cfa8dfe649785eb
partic2 <1596067968@qq.com>
2025-04-01 18:49:46 +0800

    support remote [file mapping] memory access

--- runtime_bridge/c-modules/base/pxprpc_rtbridge_base.hpp
+++ runtime_bridge/c-modules/base/pxprpc_rtbridge_base.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <pxprpc_pp.hpp>
+#include <pxprpc_ext.hpp>
 #include <queue>
 #include <functional>
 extern "C"{
@@ -8,6 +9,7 @@ extern "C"{
 }
 
 #include <iostream>
+#include <memfile.h>
 
 namespace pxprpc_rtbridge_base{
     void __callArg0Function(void *cb){
@@ -63,41 +65,89 @@ namespace pxprpc_rtbridge_base{
         auto serv=reinterpret_cast<PxpPipeServer *>(server);
         serv->onConnect(io1);
     }
-    
+    class MemoryChunk:public pxprpc::PxpObject{
+        public:
+        //1-malloc
+        //2-access
+        //3-file mapping
+        int createBy=0;
+        memfile_t mf={NULL,NULL};
+        int32_t size=0;;
+        void *base=nullptr;
+        const char *alloc(int32_t sizein){
+            size=sizein;
+            base=::malloc(size);
+            createBy=1;
+            return nullptr;
+        }
+        const char *access(void *basein,int32_t sizein){
+            base=basein;
+            size=sizein;
+            createBy=2;
+            return nullptr;
+        }
+        const char* filemap(std::string& path,std::string& mode,int32_t sizein){
+            size=sizein;
+            int nmode=0;
+            if(mode.find('r')!=std::string::npos){
+                nmode|=MEMFILE_READ;
+            }
+            if(mode.find('w')!=std::string::npos){
+                nmode|=MEMFILE_WRITE;
+            }
+            const char* err=memfile_create(path.c_str(),nmode,size,&mf);
+            base=mf.data;
+            if(err==nullptr)createBy=3;
+            return err;
+        }
+        virtual ~MemoryChunk(){
+            switch(createBy){
+                case 1:
+                ::free(base);
+                break;
+                case 3:
+                printf("MemoryChunk free");
+                memfile_close(&mf);
+                break;
+            }
+            createBy=0;
+            base=nullptr;
+        }
+    };
     bool inited=false;
     void init(){
         if(!inited){
             defaultFuncMap.add((new pxprpc::NamedFunctionPPImpl1())->init("pxprpc_pipe_pp.serve",
-            [](auto para,auto ret)->void{
+            [](auto para,auto ret)->void {
                 auto obj=new PxpPipeServer(para->nextString());
                 obj->serve();
                 ret->resolve(obj);
             })).add((new pxprpc::NamedFunctionPPImpl1())->init("pxprpc_pipe_pp.accept",
-            [](auto para,auto ret)->void{
+            [](auto para,auto ret)->void {
                 auto serv=static_cast<PxpPipeServer *>(para->nextObject());
-                serv->accept([ret](auto io)->void{
+                serv->accept([ret](auto io)->void {
                     ret->resolve(new PxpConnection(io));
                 });
             })).add((new pxprpc::NamedFunctionPPImpl1())->init("pxprpc_pipe_pp.connect",
-            [](auto para,auto ret)->void{
+            [](auto para,auto ret)->void {
                 auto servName=para->nextString();
                 ret->resolve(new PxpConnection(pxprpc_pipe_connect(servName.c_str())));
             })).add((new pxprpc::NamedFunctionPPImpl1())->init("pxprpc_pp.io_to_raw_addr",
-            [](auto para,auto ret)->void{
+            [](auto para,auto ret)->void {
                 auto ioaddr=reinterpret_cast<int64_t>(static_cast<PxpConnection *>((para->nextObject()))->io);
                 ret->resolve(ioaddr);
             })).add((new pxprpc::NamedFunctionPPImpl1())->init("pxprpc_pp.io_from_raw_addr",
-            [](auto para,auto ret)->void{
+            [](auto para,auto ret)->void {
                 ret->resolve(new PxpConnection(reinterpret_cast<struct pxprpc_abstract_io *>(para->nextLong())));
             })).add((new pxprpc::NamedFunctionPPImpl1())->init("pxprpc_pp.io_send",
-            [](auto para,auto ret)->void{
+            [](auto para,auto ret)->void {
                 auto ioaddr=static_cast<PxpConnection *>((para->nextObject()))->io;
                 auto buffer1=new pxprpc_buffer_part();
                 auto p2=para->nextBytes();
                 buffer1->bytes.length=std::get<0>(p2);
                 buffer1->bytes.base=std::get<1>(p2);
                 buffer1->next_part=nullptr;
-                auto cb=new std::function<void()>([ret,buffer1,ioaddr]()->void{
+                auto cb=new std::function<void()>([ret,buffer1,ioaddr]()->void {
                     auto err=ioaddr->get_error(ioaddr,reinterpret_cast<void *>(ioaddr->send));
                     if(err!=nullptr){
                         ret->reject(err);
@@ -108,18 +158,18 @@ namespace pxprpc_rtbridge_base{
                 });
                 ioaddr->send(ioaddr,buffer1,__callArg0Function,cb);
             })).add((new pxprpc::NamedFunctionPPImpl1())->init("pxprpc_pp.io_receive",
-            [](pxprpc::NamedFunctionPPImpl1::Parameter *para,auto ret)->void{
+            [](pxprpc::NamedFunctionPPImpl1::Parameter *para,auto ret)->void {
                 auto ioaddr=static_cast<PxpConnection *>((para->nextObject()))->io;
                 auto buffer1=new pxprpc_buffer_part();
                 buffer1->bytes.base=nullptr;
                 buffer1->bytes.length=0;
                 buffer1->next_part=nullptr;
-                auto cb=new std::function<void()>([ret,buffer1,ioaddr]()->void{
+                auto cb=new std::function<void()>([ret,buffer1,ioaddr]()->void {
                     auto err=ioaddr->get_error(ioaddr,reinterpret_cast<void *>(ioaddr->receive));
                     if(err!=nullptr){
                         ret->reject(err);
                     }else{
-                        ret->resolve(*buffer1,[ioaddr,buffer1]()->void{
+                        ret->resolve(*buffer1,[ioaddr,buffer1]()->void {
                             ioaddr->buf_free(buffer1->bytes.base);
                         });
                     }
@@ -127,12 +177,56 @@ namespace pxprpc_rtbridge_base{
                 });
                 ioaddr->receive(ioaddr,buffer1,__callArg0Function,cb);
             })).add((new pxprpc::NamedFunctionPPImpl1())->init("pxprpc_rtbridge_host.new_tcp_rpc_server",
-            [](auto para,auto ret)->void{
+            [](auto para,auto ret)->void {
                 auto ser=para->asSerializer();
                 auto host=ser->getString();
                 auto port=ser->getInt();
                 auto tcp=new pxprpc_rtbridge_host::TcpPxpRpcServer(host,port);
                 ret->resolve(tcp);
+            })).add((new pxprpc::NamedFunctionPPImpl1())->init("pxprpc_rtbridge.memory_alloc",
+            [](pxprpc::NamedFunctionPPImpl1::Parameter *para, pxprpc::NamedFunctionPPImpl1::AsyncReturn *ret)->void {
+                auto size=para->nextInt();
+                auto chunk=new MemoryChunk();
+                chunk->alloc(size);
+                ret->resolve(chunk);
+            })).add((new pxprpc::NamedFunctionPPImpl1())->init("pxprpc_rtbridge.memory_access",
+            [](pxprpc::NamedFunctionPPImpl1::Parameter *para, pxprpc::NamedFunctionPPImpl1::AsyncReturn *ret)->void {
+                auto addr=para->nextLong();
+                auto size=para->nextInt();
+                auto chunk=new MemoryChunk();
+                chunk->access(reinterpret_cast<void *>(addr),size);
+                ret->resolve(chunk);
+            })).add((new pxprpc::NamedFunctionPPImpl1())->init("pxprpc_rtbridge.memory_read",
+            [](pxprpc::NamedFunctionPPImpl1::Parameter *para, pxprpc::NamedFunctionPPImpl1::AsyncReturn *ret)->void {
+                auto chunk=static_cast<MemoryChunk *>(para->nextObject());
+                ret->resolve(chunk->base,chunk->size);
+            })).add((new pxprpc::NamedFunctionPPImpl1())->init("pxprpc_rtbridge.memory_write",
+            [](pxprpc::NamedFunctionPPImpl1::Parameter *para, pxprpc::NamedFunctionPPImpl1::AsyncReturn *ret)->void {
+                auto chunk=static_cast<MemoryChunk *>(para->nextObject());
+                auto data=para->nextBytes();
+                memmove(chunk->base,std::get<1>(data),std::get<0>(data));
+                ret->resolve();
+            })).add((new pxprpc::NamedFunctionPPImpl1())->init("pxprpc_rtbridge.memory_info",
+            [](pxprpc::NamedFunctionPPImpl1::Parameter *para, pxprpc::NamedFunctionPPImpl1::AsyncReturn *ret)->void {
+                auto chunk=static_cast<MemoryChunk *>(para->nextObject());
+                auto ser=new pxprpc::Serializer();
+                ser->prepareSerializing(16);
+                ser->putLong(reinterpret_cast<int64_t>(chunk->base));
+                ser->putInt(chunk->size);
+                ret->resolve(ser);
+            })).add((new pxprpc::NamedFunctionPPImpl1())->init("pxprpc_rtbridge.memory_mapfile",
+            [](pxprpc::NamedFunctionPPImpl1::Parameter *para, pxprpc::NamedFunctionPPImpl1::AsyncReturn *ret)->void {
+                auto path=para->nextString();
+                auto mode=para->nextString();
+                auto size=para->nextInt();
+                auto fm=new MemoryChunk();
+                auto err=fm->filemap(path,mode,size);
+                if(err!=nullptr){
+                    ret->reject(err);
+                    delete fm;
+                }else{
+                    ret->resolve(fm);
+                }
             }));
         }
     }
---
+++ runtime_bridge/include/memfile.h
@@ -0,0 +1,219 @@
+
+
+/*
+   Pathed from https://github.com/COKPOWEHEU/memfile
+*/
+
+#ifndef __MEMFILE_H__
+#define __MEMFILE_H__
+
+#define MEMFILE_READ      0x01
+#define MEMFILE_WRITE     0x02
+#define MEMFILE_READWRITE (MEMFILE_READ | MEMFILE_WRITE)
+#define MEMFILE_REWRITE   0x04
+#define MEMFILE_FASTREWRITE 0x08
+
+
+struct memfile_t{
+  /* internal data */
+  void *intdata;
+  /* map result address. */
+  void *data;
+};
+
+const char* memfile_create(const char filename[], unsigned char flags, unsigned long size, struct memfile_t *res);
+const char* memfile_close(struct memfile_t *mem);
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/*
+ *  Linux
+ */
+#if defined(linux) || defined(__linux) || defined(__linux__) || defined(__GNU__) || defined(__GLIBC__)
+#include <sys/mman.h>
+typedef struct{
+  int fd;
+  unsigned long size;
+}memfile_d;
+
+const char* memfile_create(const char filename[], unsigned char flags, unsigned long size, struct memfile_t *res){
+  int fd;
+  void *mem=NULL;
+  int i, prot=0, acc=0;
+
+  res->intdata = NULL;
+  res->data = NULL;
+
+  if(flags & MEMFILE_READ){
+    prot |= PROT_READ;
+    acc  |= S_IRUSR | S_IRGRP | S_IROTH;
+    i = O_RDONLY;
+  }
+  if(flags & MEMFILE_WRITE){
+    i = O_CREAT;
+    prot |= PROT_WRITE;
+    acc  |= S_IWUSR;
+    if(flags & MEMFILE_READ){
+      i |= O_RDWR;
+    }else{
+      i |= O_WRONLY;
+    }
+  }
+
+  fd = open(filename, i, acc);
+  if(fd < 0)return "open failed";
+  if(flags & MEMFILE_WRITE){
+    if(flags & MEMFILE_REWRITE){
+      for(acc=0; acc<size; acc++)write(fd, " ", 1);
+    }else if(flags & MEMFILE_FASTREWRITE){
+      lseek(fd, size, SEEK_SET);
+      write(fd, " ", 1);
+    }
+  }
+  mem = mmap(NULL, size, prot, MAP_SHARED, fd, 0);
+  if(mem == NULL){
+    close(fd);
+    return "mmap failed";
+  }
+
+  res->intdata = malloc(sizeof(memfile_d));
+  ((memfile_d*)(res->intdata))->fd = fd;
+  ((memfile_d*)(res->intdata))->size = size;
+  res->data = mem;
+  
+  return NULL;
+}
+const char* memfile_close(struct memfile_t *mem){
+  int fd;
+  unsigned long size;
+  if(mem == NULL) NULL;
+  if(mem->intdata == NULL)return NULL;
+  fd = ((memfile_d*)(mem->intdata))->fd;
+  size = ((memfile_d*)(mem->intdata))->size;
+  munmap(mem->data, size);
+  close(fd);
+  free(mem->intdata);
+  mem->intdata = NULL;
+  mem->data = NULL;
+  return NULL;
+}
+/*
+ *  Win 32
+ */
+#elif defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
+ //Win32
+#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>
+typedef struct{
+  HANDLE fd, fm;
+}memfile_d;
+
+//Caller take response to delete the buffer.
+wchar_t* getWideChar(const std::string &utf8Str){
+  int wideCharCount = MultiByteToWideChar(CP_UTF8, 0, utf8Str.c_str(), utf8Str.length(), nullptr, 0);
+  if (wideCharCount == 0) {
+      return nullptr;
+  }
+
+  wchar_t* wideCharStr = new wchar_t[wideCharCount];
+
+  if (MultiByteToWideChar(CP_UTF8, 0, utf8Str.c_str(), utf8Str.length(), wideCharStr, wideCharCount) == 0) {
+      delete[] wideCharStr;
+      return nullptr;
+  }
+  return wideCharStr;
+}
+
+const char* memfile_create(const char filename[], unsigned char flags, unsigned long size, struct memfile_t *res){
+  HANDLE fd, fm;
+  void *mem;
+  int cflag=0, mflag=0, vflag=0;
+  res->intdata = NULL;
+  res->data = NULL;
+  wchar_t *widename=NULL;
+  int32_t widenamelen=0;
+  int32_t utf8namelen=strlen(filename)+1;
+  widenamelen=MultiByteToWideChar(CP_UTF8,0,filename,utf8namelen,NULL,0);
+  if(widenamelen==0){
+    return "MultiByteToWideChar failed";
+  }
+  widename=(wchar_t *)malloc(sizeof(wchar_t)*widenamelen);
+  MultiByteToWideChar(CP_UTF8,0,filename,utf8namelen,widename,widenamelen);
+
+  
+  if(flags & MEMFILE_READ){
+    cflag |= GENERIC_READ;
+    mflag = PAGE_READONLY;
+    vflag |= FILE_MAP_READ;
+  }
+  if(flags & MEMFILE_WRITE){
+    cflag |= GENERIC_WRITE;
+    mflag = PAGE_READWRITE;
+    vflag |= FILE_MAP_WRITE;
+  }
+  
+  if(flags & MEMFILE_WRITE){
+    fd = CreateFileW(widename, cflag, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+  }else{
+    fd = CreateFileW(widename, cflag, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+  }
+  free(widename);
+  
+  
+  if(fd == INVALID_HANDLE_VALUE)return "CreateFile failed";
+
+  if(flags & MEMFILE_WRITE){
+    if(flags & MEMFILE_REWRITE){
+      char sample = ' ';
+      long unsigned int cnt1=0;
+      for(cflag=0; cflag<size;cflag++)WriteFile(fd, &sample, 1, &cnt1, NULL);
+    }else if(flags & MEMFILE_FASTREWRITE){
+      SetFilePointer(fd, size, NULL, FILE_BEGIN);
+    }
+  }
+  
+  fm = CreateFileMapping(fd, NULL, mflag, 0, size, NULL);
+  if(fm == NULL){
+    CloseHandle(fm);
+    return "CreateFileMapping failed";
+  }
+  mem = MapViewOfFile(fm, vflag, 0, 0, size);
+  if(mem == NULL){
+    CloseHandle(fm);
+    CloseHandle(fd);
+    return "MapViewOfFile failed";
+  }
+  res->intdata = malloc(sizeof(memfile_d));
+  ((memfile_d*)(res->intdata))->fd = fd;
+  ((memfile_d*)(res->intdata))->fm = fm;
+  res->data = mem;
+  
+  return NULL;
+}
+const char* memfile_close(struct memfile_t *mem){
+  if(mem == NULL)return NULL;
+  if(mem->intdata == NULL)return NULL;
+  HANDLE fd, fm;
+  fd = ((memfile_d*)(mem->intdata))->fd;
+  fm = ((memfile_d*)(mem->intdata))->fm;
+  UnmapViewOfFile(mem->data);
+  CloseHandle(fm);
+  CloseHandle(fd);
+  free(mem->intdata);
+  return NULL;
+}
+ /*
+  *  Other systems (unsupported)
+  */
+#else
+const char* memfile_create(const char filename[], unsigned char flags, unsigned long size, struct memfile_t *res){
+  return "Not implemeted";
+}
+const char* memfile_close(struct memfile_t *mem){
+  return "Not implemeted";
+}
+#endif
+
+#endif
